2dd61a96e6157bf4972b467e57eed12014978d50
ArjanSchouten <arjan.schouten@cgi.com>
2021-05-27 17:37:15 +0200

    fix test

--- __tests__/randomNumber.test.js
+++ __tests__/randomNumber.test.js
@@ -2,8 +2,7 @@ const {generateRandomNumber} = require('../randomNumber');
 
 describe('generateRandomNumber', () => {
    test('should generate a random number', () => {
-       // FIXME, the randomNumber variable should be const randomNumber = generateRandomNumber();
-      const randomNumber = 0;
+      const randomNumber = generateRandomNumber();
       expect(randomNumber).toBeGreaterThan(0);
    });
 });
